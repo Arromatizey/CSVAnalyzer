199f9c3e248431f5b887256e7e54cadeb75486cf
animaSPW <94543307+animaSPW@users.noreply.github.com>
2025-02-06 10:00:15 +0100

    added git ignore

--- src/app/aws-config.ts
+++ src/app/aws-config.ts
@@ -1,6 +1,7 @@
+import { environment } from "../environments/environment";
 export const s3Config = {
-    accessKeyId: 'AKIASU566JZRVAQBRHUN',
-    secretAccessKey: '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',
-    region: 'eu-north-1',
-    bucketName: 'csv-analysis-bucket1',
+    accessKeyId: environment.accessKeyId,
+    secretAccessKey: environment.secretAccessKey,
+    region: environment.region,
+    bucketName: environment.bucketName
   };
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    accessKeyId: 'AKIASU566JZRVAQBRHUN',
+    secretAccessKey: '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',
+    region: 'eu-north-1',
+    bucketName: 'csv-analysis-bucket1',
+  };
